4470e8ad6ad2ed10a0c26f8148044db78e8d7946
Codewithboyy <mittal88832@gmail.com>
2026-06-07 10:06:51 +0530

    Configure VaultDetector for 1.21.8

--- src/client/java/com/spydr6307/client/VaultDetectorClient.java
+++ src/client/java/com/spydr6307/client/VaultDetectorClient.java
@@ -1,13 +1,11 @@
 package com.spydr6307.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import com.spydr6307.client.event.ClientEvents;
 
 public class VaultDetectorClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
         System.out.println("[VaultDetector] Client Loaded!");
-        ClientEvents.register();
     }
 }
