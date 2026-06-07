6d03ae3d06de497498dd44a9f6bb6c3854b06b6f
Codewithboyy <mittal88832@gmail.com>
2026-06-07 10:39:59 +0530

    Restore clean client

--- src/client/java/com/spydr6307/client/VaultDetectorClient.java
+++ src/client/java/com/spydr6307/client/VaultDetectorClient.java
@@ -1,14 +1,11 @@
 package com.spydr6307.client;
 
-import com.spydr6307.client.event.ClientEvents;
-import com.spydr6307.client.hud.VaultHud;
 import net.fabricmc.api.ClientModInitializer;
 
 public class VaultDetectorClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ClientEvents.register();
-        VaultHud.register();
+        System.out.println("[VaultDetector] Client initialized");
     }
 }
